2f2806d4d78070f683710ff14a771ac812309db7
Annie Ulanska <a.ulanskaxxi@gmail.com>
2023-07-19 16:40:43 +0100

    task 2 & part of taks 3

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+// get ul ingrediens
+const ulList = document.querySelector("#ingredients");
+// create li emement
+const liList = document.createElement("li");
+// add class .item
+liList.className = "item";
+// add text of first li element
+liList.textContent = "Potatoes";
+// dodaj el klasy item do ul list 
+ulList.append(liList);
+// add array of the rest of elements
+const updateList = ingredients.slice(1, 6).map((ingredient) => 
+`<li class="item">${ingredient}</li>`).join("");
+ulList.insertAdjacentHTML("beforeend", updateList);
--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,38 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+// find gallery ul
+const gallery = document.querySelector(".gallery");
+// create li element
+const liItem = document.createElement("li");
+gallery.append(liItem);
+// create img element
+const image = document.createElement("img");
+// add src attribute
+image.setAttribute("src", "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260")
+// add alt attribute
+image.setAttribute("alt", "White and Black Long Fur Cat");
+// dodaj <li><img></li> do ul.gallery
+liItem.appendChild(image);
+
+
+// add all images
+const galImages = images.map((img) => 
+`<li>${img}</li>`).join("");
+liItem.insertAdjacentHTML("beforeend", galImages);
+console.log(galImages)
+
+
+
+// for(let i = 0; i < images.length; i++) {
+//   let newImg = document.createElement("img");
+//   newImg.setAttribute("alt", images[i].alt);
+//   newImg.src = images[i].url;
+
+//   let liItem = document.createElement("li")
+//   liItem.appendChild(newImg)
+
+//   document.getElementsByClassName('.gallery').appendChild(liItem)
+//   console.log(liItem)
+// }
