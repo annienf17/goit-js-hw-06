34f3a7ee2538770579f9de72b8fd3af73b1bfcad
Annie Ulanska <a.ulanskaxxi@gmail.com>
2023-07-21 21:36:19 +0100

    task 4

--- js/task-04.js
+++ js/task-04.js
@@ -3,14 +3,22 @@
 let counterValue = 0;
 // counter in HTML
 let value = document.getElementById("value");
-// select button
-const button = document.querySelector("button");
 
-const handleClick = () => {
+// select button
+const decrementButton = document.querySelector('[data-action="decrement"]');
+const incrementButton = document.querySelector('[data-action="increment"]');
+
+const handleClickDecr = () => {
     counterValue--;
 // assign to HTML value counterValue
     value.innerHTML = counterValue;    
 };
-
-button.addEventListener("click", handleClick);
+const handleClickIncr = () => {
+    counterValue++;
+// assign to HTML value counterValue
+    value.innerHTML = counterValue;    
+};
+// callback function
+decrementButton.addEventListener("click", handleClickDecr);
+incrementButton.addEventListener("click", handleClickIncr);
 
