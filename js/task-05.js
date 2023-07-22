b511fde97f342d4c6e9eee8110b79e56e5c02f07
Annie Ulanska <a.ulanskaxxi@gmail.com>
2023-07-22 17:43:19 +0100

    task 5

--- js/task-05.js
+++ js/task-05.js
@@ -2,12 +2,17 @@
 // selelct elements by id
 const textInput = document.getElementById("name-input");
 const output = document.getElementById("name-output");
+
 console.log(output.innerHTML.length);
 console.log(output.innerHTML);
 
 // when input written
 const handleInputText = (event) => {
-output.innerHTML = event.currentTarget.value;  
+     if(textInput.value.length > 0) {
+        output.innerHTML = event.currentTarget.value;
+    } else if(textInput.value.length < 1) {
+        output.innerHTML = output.value;     
+    }
 };
 textInput.addEventListener("input", handleInputText);
 
