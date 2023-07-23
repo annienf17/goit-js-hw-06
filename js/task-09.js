061f1955e4aba205d3d9be4f1c7d63e2bef6307e
Annie Ulanska <a.ulanskaxxi@gmail.com>
2023-07-23 13:29:31 +0100

    task 9 print color

--- js/task-09.js
+++ js/task-09.js
@@ -1,10 +1,15 @@
 const buttonColor = document.querySelector(".change-color");
+const printColor = document.querySelector(".color");
 
-const getRandomHexColor = () => {
+
+const getRandomHexColor = (event) => {
   document.body.style.background = 
    `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+    printColor.innerHTML = document.body.style.background;
 }
 
 buttonColor.addEventListener("click", getRandomHexColor);
+
+
--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+
+const createBoxes = (amount) => {
+const boxes = document.getElementById("boxes");
+const div = document.createElement("div");
+boxes.append(div*amount);
+
+}
