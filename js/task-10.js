adaff595d28e8c81743ab4b696214ee743ff2e79
Annie Ulanska <a.ulanskaxxi@gmail.com>
2023-07-26 20:28:44 +0100

    task 10 without 10px extends

--- js/task-10.js
+++ js/task-10.js
@@ -1,38 +1,50 @@
+'use strrict';
+// random colors
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
 
-// create div in boxex
-const boxes = document.getElementById("boxes");
-
-// take value from input
-const inputField = document.querySelector("input");
-const inputValue = inputField.value;
-
-
 // buttons
 const createButton = document.querySelector('[data-create]');
 const destroyButton = document.querySelector('[data-destroy]');
 
-const handleClickCreate = () => {
-  // function adds div amount from input
-for (let i = 0; i < inputValue; i++){
+ // function adds div amount from input
+function createBoxes(amount) {
+  // create div in boxex
+const boxes = document.getElementById("boxes");
+console.log(boxes)
+
+// take value from input
+const inputField = document.querySelector("input");
+const inputValue = inputField.value;
+amount = inputValue;
+
+// create div 
+for (let i = 0; i < amount; i++){
   const div = document.createElement("div");
   div.style.width = "30px";
   div.style.height = "30px";
   div.style.backgroundColor = getRandomHexColor();
-  const divAdd = boxes.append(div);
+  boxes.append(div);
+
+  // array of created divs
+  const divsArray = [];
+  divsArray.push(div);
+  console.log(divsArray);
+
+  // extend every next div by 10px
+  let result = divsArray.map(d => d.style.width += "10px");
+  console.log(result);
 }
 }
 // remove all child nodes from a list
-const handleClickDestroy = () => {
+function destroyBoxes() {
   const list = document.getElementById("boxes");
   while (list.hasChildNodes()) {
     list.removeChild(list.firstChild);
   }
-  
 }
-createButton.addEventListener("click", handleClickCreate);
-destroyButton.addEventListener("click", handleClickDestroy);
+createButton.addEventListener("click", createBoxes);
+destroyButton.addEventListener("click", destroyBoxes);
