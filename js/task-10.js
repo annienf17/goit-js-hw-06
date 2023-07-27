bca9bbc5b02524920db9566e351362473c50aadf
Annie Ulanska <a.ulanskaxxi@gmail.com>
2023-07-27 16:09:12 +0100

    task 10 no 10px resizing yet

--- js/task-10.js
+++ js/task-10.js
@@ -5,38 +5,26 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-
 // buttons
 const createButton = document.querySelector('[data-create]');
 const destroyButton = document.querySelector('[data-destroy]');
 
  // function adds div amount from input
 function createBoxes(amount) {
-  // create div in boxex
+// create div in boxex
 const boxes = document.getElementById("boxes");
-console.log(boxes)
-
 // take value from input
 const inputField = document.querySelector("input");
-const inputValue = inputField.value;
-amount = inputValue;
+amount = inputField.value;
 
 // create div 
 for (let i = 0; i < amount; i++){
   const div = document.createElement("div");
-  div.style.width = "30px";
-  div.style.height = "30px";
+  let divW = div.style.width = "30px";
+  let divH = div.style.height = "30px";
   div.style.backgroundColor = getRandomHexColor();
   boxes.append(div);
 
-  // array of created divs
-  const divsArray = [];
-  divsArray.push(div);
-  console.log(divsArray);
-
-  // extend every next div by 10px
-  let result = divsArray.map(d => d.style.width += "10px");
-  console.log(result);
 }
 }
 // remove all child nodes from a list
