799e6d3c4ec1511441c1348ecb8e02769649e60b
Annie Ulanska <a.ulanskaxxi@gmail.com>
2023-07-28 16:12:04 +0100

    task 10 click jump to beginning

--- js/task-10.js
+++ js/task-10.js
@@ -1,4 +1,4 @@
-'use strrict';
+'use strict';
 // random colors
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
@@ -9,14 +9,14 @@ function getRandomHexColor() {
 const createButton = document.querySelector('[data-create]');
 const destroyButton = document.querySelector('[data-destroy]');
 
- // function adds div amount from input
+// function adds div amount from input
 function createBoxes(amount) {
 // create div in boxex
 const boxes = document.getElementById("boxes");
 // take value from input
 const inputField = document.querySelector("input");
 amount = inputField.value;
-
+// first div size
 let size = 30;
 // create div 
 for (let i = 0; i < amount; i++){
@@ -27,14 +27,16 @@ div.style.backgroundColor = getRandomHexColor();
 boxes.append(div);
 // increase by 10px
 size += 10;
-}}
+console.log(size)
+}};
+
 // remove all child nodes from a list
 function destroyBoxes() {
   const list = document.getElementById("boxes");
   while (list.hasChildNodes()) {
     list.removeChild(list.firstChild);
-  }
-}
+  }};
+
 // call event by click on button
 createButton.addEventListener("click", createBoxes);
 destroyButton.addEventListener("click", destroyBoxes);
