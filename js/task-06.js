670d950ac845535da10c5fe52015e50595cf7e8c
Annie Ulanska <a.ulanskaxxi@gmail.com>
2023-07-28 16:55:33 +0100

    task 6 update

--- js/task-06.js
+++ js/task-06.js
@@ -1,14 +1,16 @@
-'use strict'
-const textInput = document.querySelector("input");
+'use strict';
+
+const textInput = document.querySelector("#validation-input");
 
 const handleText = () => {
-    if(textInput.value.length === 6){
-       textInput.classList.add('valid') 
-    } else if (textInput.value.length !== 6) {
-        textInput.classList.add('invalid') 
+    if(textInput.value.length !== 6){
+       textInput.classList.add('invalid') 
+
+    } else if (textInput.value.length === 6) {
+        textInput.classList.remove('invalid')
+        textInput.classList.add('valid')
     }
 }
-
 textInput.addEventListener("focus", handleText);
 
 
--- js/task-10.js
+++ js/task-10.js
@@ -9,6 +9,9 @@ function getRandomHexColor() {
 const createButton = document.querySelector('[data-create]');
 const destroyButton = document.querySelector('[data-destroy]');
 
+// first div size
+let size = 30;
+
 // function adds div amount from input
 function createBoxes(amount) {
 // create div in boxex
@@ -16,18 +19,16 @@ const boxes = document.getElementById("boxes");
 // take value from input
 const inputField = document.querySelector("input");
 amount = inputField.value;
-// first div size
-let size = 30;
 // create div 
-for (let i = 0; i < amount; i++){
+for (let i = 0; i < amount; ++i){
   const div = document.createElement("div");
-div.style.width = `${size}px`;
-div.style.height = `${size}px`;
+let divW = div.style.width = `${size}px`;
+let divH = div.style.height = `${size}px`;
 div.style.backgroundColor = getRandomHexColor();
 boxes.append(div);
+console.log(divW, divH)
 // increase by 10px
 size += 10;
-console.log(size)
 }};
 
 // remove all child nodes from a list
@@ -35,6 +36,7 @@ function destroyBoxes() {
   const list = document.getElementById("boxes");
   while (list.hasChildNodes()) {
     list.removeChild(list.firstChild);
+   
   }};
 
 // call event by click on button
